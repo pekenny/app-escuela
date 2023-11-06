83480fef084be58e2d4ee46aebb32bb99e13e5c7
Pablo Kenny <pablokenny20@gmail.com>
2023-11-06 12:51:13 -0300

    Login

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,10 +1,31 @@
-import {ref} from 'vue'
-import { defineStore } from 'pinia'
+import { ref, reactive } from "vue";
+import { defineStore } from "pinia";
+import { useRouter } from "vue-router";
 
-export const useUserStore = defineStore('user', () => {
-    const login = ref(false)
+export const useUserStore = defineStore("user", () => {
+  const login = ref(false);
+  const user = reactive({
+    username: "",
+    password: "",
+  });
 
-    return {
-       login
+  const router = useRouter();
+
+  const validationLogin = () => {
+    if (user.username === "admin" && user.password === "admin") {
+      login.value = true;
+    //    redirigir a home con router
+  
+    router.push({ name: "home" });
     }
-})
+    else {
+      login.value = false;
+    }
+  }
+
+  return {
+    login,
+    user,
+    validationLogin
+  };
+});
