02d4b9b7c530d25185ceb79e4c65bbd4ae55b3fc
Pablo Kenny <pablokenny20@gmail.com>
2023-11-24 14:54:11 -0300

    Login con Api terminado

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,7 +1,7 @@
 import { ref, reactive, watch} from "vue";
 import { defineStore } from "pinia";
 import { useRouter } from "vue-router";
-
+import axios from "axios";
 
 export const useUserStore = defineStore("user", () => {
   const login = ref(false);
@@ -13,18 +13,36 @@ export const useUserStore = defineStore("user", () => {
  
   const router = useRouter();
 
-
-  const validationLogin = () => {
-    if (user.username === "admin" && user.password === "admin") {
-
-      // crear data en local storage    
-      localStorage.setItem("data", JSON.stringify(login));      
-      //    redirigir a home con router
-      router.push({ name: "home" });
-    } else {
+  // enviar datos por post
+  const loginData = async () => {
+    try {
+      const request = await axios.post(
+        "http://localhost:3000/api/login",
+        user
+      );
+      console.log(request.data);
+      if (request.data.message === true) {
+        login.value = true;
+        localStorage.setItem("data", JSON.stringify(request.data));
+        router.push({ name: "home" });
+      }
+    } catch (error) {
+      console.error(error);
       login.value = false;
     }
-  };
+  }
+
+  // const validationLogin = () => {
+  //   if (user.username === "admin" && user.password === "admin") {
+
+  //     // crear data en local storage    
+  //     localStorage.setItem("data", JSON.stringify(login));      
+  //     //    redirigir a home con router
+  //     router.push({ name: "home" });
+  //   } else {
+  //     login.value = false;
+  //   }
+  // };
 
   // cerrar sesion
   const logout = () => {
@@ -43,7 +61,8 @@ export const useUserStore = defineStore("user", () => {
   return {
     login,
     user,
-    validationLogin,
+    loginData,
+    // validationLogin,
     logout
     
   };
